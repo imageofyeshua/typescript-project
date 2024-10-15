8ec95b449edb4237f9323e475aa7ccedd9c15592
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-15 10:33:45 +0900

    Added GoogleMap

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,15 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from "./User";
+import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
-const user = new User();
+// const user = new User();
 
-console.log(user);
+// console.log(user);
+
+// const company = new Company();
+
+// console.log(company);
+
+const customMap = new CustomMap("map");
